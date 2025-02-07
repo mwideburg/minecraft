bf18529b431dd463458a3a9ea87fd808780947bc
mjwideburg <130396236+mjwideburg@users.noreply.github.com>
2025-02-07 17:02:42 -0500

    feat(player): add player camera and controls (#3)
    * update sun
    
    * add player movement
    
    * working player

--- scripts/blocks.js
+++ scripts/blocks.js
@@ -5,6 +5,8 @@ const textureLoader = new THREE.TextureLoader()
 function loadTexture(path){
     const texture = textureLoader.load(path);
     texture.colorSpace = THREE.SRGBColorSpace;
+    texture.minFilter = THREE.NearestFilter
+    texture.magFilter = THREE.NearestFilter
     return texture
 }
 
--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@ import { OrbitControls } from 'three/examples/jsm/Addons.js';
 import Stats from 'three/examples/jsm/libs/stats.module.js';
 import { World } from './world';
 import { createUI } from './ui';
+import { Player } from './player';
 
 const stats = new Stats()
 document.body.append(stats.dom)
@@ -12,15 +13,17 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0x80a0e0)
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
 document.body.appendChild(renderer.domElement)
 
 // Camera Setup
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight);
-camera.position.set(2, 2, 2);
-camera.lookAt(0, 0, 0)
+const orbitCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight);
+orbitCamera.position.set(2, 2, 2);
+orbitCamera.lookAt(0, 0, 0)
 
 // Add Controls
-const controls = new OrbitControls(camera, renderer.domElement)
+const controls = new OrbitControls(orbitCamera, renderer.domElement)
 controls.target.set(16, 0, 16)
 controls.update()
 
@@ -30,14 +33,24 @@ const world = new World()
 world.generate()
 scene.add(world)
 
-function setUpLights(){
-    const light1 = new THREE.DirectionalLight()
-    light1.position.set(1, 1, 1)
-    scene.add(light1)
+const player = new Player(scene)
 
-    const light2 = new THREE.DirectionalLight()
-    light2.position.set(-1, 1, -0.5)
-    scene.add(light2)
+function setUpLights() {
+    const sun = new THREE.DirectionalLight()
+    sun.position.set(50, 50, 50);
+    sun.castShadow = true;
+    sun.shadow.camera.left = -50
+    sun.shadow.camera.right = 50
+    sun.shadow.camera.bottom = -50
+    sun.shadow.camera.top = 50
+    sun.shadow.camera.near = 0.1
+    sun.shadow.camera.far = 100
+    sun.shadow.bias = -.0005
+    sun.shadow.mapSize = new THREE.Vector2(512, 512)
+    scene.add(sun)
+
+    const shadowHelper = new THREE.CameraHelper(sun.shadow.camera)
+    scene.add(shadowHelper)
 
     const light3 = new THREE.AmbientLight()
     light3.intensity = 0.1
@@ -45,19 +58,28 @@ function setUpLights(){
 }
 
 // Render Loop
-function animate(){
+let previousTime = performance.now()
+function animate() {
+    const currentTime = performance.now();
+    const dt = (currentTime - previousTime) / 1000;
     requestAnimationFrame(animate)
     stats.update()
-    renderer.render(scene, camera)
+
+    player.applyInputs(dt)
+    renderer.render(scene, player.controls.isLocked ? player.camera : orbitCamera)
+
     window.addEventListener("resize", () => {
-        camera.aspect = window.innerWidth / window.innerHeight
-        camera.updateProjectionMatrix()
+        orbitCamera.aspect = window.innerWidth / window.innerHeight
+        orbitCamera.updateProjectionMatrix()
+        player.camera.aspect = window.innerWidth / window.innerHeight
+        player.camera.updateProjectionMatrix()
         renderer.setSize(window.innerWidth, window.innerHeight)
     })
+    previousTime = currentTime
 }
 
 setUpLights()
-createUI(world)
+createUI(world, player)
 animate()
 
 
